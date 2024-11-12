3b425a5189f8ba542dd431c00b3dcb89cc249295
Gerk <gerkinfeltser@pm.me>
2024-11-12 08:25:18 -0600

    Include main.js and main.js.map for compatibility and debugging

---
+++ main.js
@@ -0,0 +1,35 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const obsidian_1 = require("obsidian");
+class TrueSelectAllPlugin extends obsidian_1.Plugin {
+    onload() {
+        return __awaiter(this, void 0, void 0, function* () {
+            // Add the "True Select All" command with a slashtag
+            this.addCommand({
+                id: 'true-select-all',
+                name: 'True Select All',
+                editorCallback: (editor) => {
+                    const doc = editor.getDoc();
+                    const start = { line: 0, ch: 0 };
+                    const lastLine = doc.lastLine();
+                    const end = { line: lastLine, ch: doc.getLine(lastLine).length };
+                    editor.setSelection(start, end);
+                },
+                slashtags: ["select all", "select entire note", "true select all"], // Adds tags for slash command search
+            });
+        });
+    }
+    onunload() {
+        console.log('True Select All Plugin unloaded.');
+    }
+}
+exports.default = TrueSelectAllPlugin;
